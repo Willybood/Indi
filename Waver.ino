b9920cfb293341c88820cc0f2706a9caec8d437a
Willybood <Willybood@yahoo.co.uk>
2014-08-04 23:24:32 +0100

    Basic Cardboard prototype code is done. Requires some more thorough testing though when we can.

--- Waver.ino
+++ Waver.ino
@@ -1,6 +1,8 @@
 // The source for the Android application can be found at the following link: https://github.com/Lauszus/ArduinoBlinkLED
 // The code for the Android application is heavily based on this guide: http://allaboutee.com/2011/12/31/arduino-adk-board-blink-an-led-with-your-phone-code-and-explanation/ by Miguel
 #include <adk.h>
+#include <Servo.h>
+#include <QueueList.h> // GPLv3
 
 USB Usb;
 ADK adk(&Usb, "DorsetEggs", // Manufacturer Name
@@ -9,22 +11,40 @@ ADK adk(&Usb, "DorsetEggs", // Manufacturer Name
               "1.0", // Version
               "http://www.tkjelectronics.dk/uploads/ArduinoBlinkLED.apk", // URL (web page to visit if no installed apps support the accessory)
               "123456789"); // Serial Number (optional)
-
-#define LED LED_BUILTIN // Use built in LED  - note that pin 13 is occupied by the SCK pin on a normal Arduino (Uno, Duemilanove etc.), so use a different pin
-
-uint32_t timer;
 boolean connected;
+enum ServoTypes {DLARM = 0, ULARM, DRARM, URARM, NUMOFSERVOS};
+Servo servos[NUMOFSERVOS];
+#define SEVO_PIN_DEFINITION {/*DLARM = */3, /*ULARM = */5, /*DRARM = */6, /*URARM = */11}
+#define MAX_SERVO_ROTATION 180
+uint32_t start = 0;
+uint32_t finished = 0;
+uint32_t timeBetweenProcessing = 0;
+
+struct KeyFrame {
+  uint8_t degreesToReach; // 0 to 180, 0 is straight down
+  //The max amount of time that an animation keyframe can take is 65 seconds.
+  //We can up it later if we feel like it by upping these variables to uint32_t
+  uint16_t timeToPosition;//Time in ms to reach this point
+  uint16_t timeSpent;//Time spent so far reaching this point
+};
+struct KeyframePacket {
+  uint8_t servoToApplyTo;
+  //The below two are used to instantiate a KeyFrame object
+  uint8_t degreesToReach;
+  uint16_t timeToPosition;
+};
+QueueList <KeyFrame> servoAnimations[NUMOFSERVOS];
 
 void setup() {
-  //pinMode( 7, OUTPUT); digitalWrite( 7, HIGH);
   Serial.begin(115200);
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
   if (Usb.Init() == -1) {
     Serial.print("\r\nOSCOKIRQ failed to assert");
     while (1); // halt
   }
-  pinMode(LED, OUTPUT);
-  Serial.print("\r\nArduino Blink LED Started");
+  Serial.print("\r\nWaver connected");
+  
+  setupServos();
 }
 
 void loop() {
@@ -33,37 +53,97 @@ void loop() {
   if (adk.isReady()) {
     if (!connected) {
       connected = true;
-      Serial.print(F("\r\nConnected to accessory"));
-    }
-
-    uint8_t msg[1];
-    uint16_t len = sizeof(msg);
-    uint8_t rcode = adk.RcvData(&len, msg);
-    if (rcode && rcode != hrNAK) {
-      Serial.print(F("\r\nData rcv: "));
-      Serial.print(rcode, HEX);
-    } else if (len > 0) {
-      Serial.print(F("\r\nData Packet: "));
-      Serial.print(msg[0]);
-      digitalWrite(LED, msg[0] ? HIGH : LOW);
-    }
-
-    if (millis() - timer >= 1000) { // Send data every 1s
-      timer = millis();
-      rcode = adk.SndData(sizeof(timer), (uint8_t*)&timer);
-      if (rcode && rcode != hrNAK) {
-        Serial.print(F("\r\nData send: "));
-        Serial.print(rcode, HEX);
-      } else if (rcode != hrNAK) {
-        Serial.print(F("\r\nTimer: "));
-        Serial.print(timer);
-      }
+      Serial.print(F("\r\nConnected to phone"));
     }
   } else {
     if (connected) {
       connected = false;
-      Serial.print(F("\r\nDisconnected from accessory"));
-      digitalWrite(LED, LOW);
+      Serial.print(F("\r\nDisconnected from phone"));
+    }
+  }
+  recieveAnimationCommand();
+  processServos();
+}
+
+void recieveAnimationCommand()
+{
+  if(connected)
+  {
+    size_t len = sizeof(KeyframePacket);
+    uint8_t msg[len];
+    uint8_t rcode = adk.RcvData(&len, msg);
+    if (rcode && rcode != hrNAK) {
+      //Data error
+      Serial.print(F("\r\nData rcv error: "));
+      Serial.print(rcode, HEX);
+    } else if (len => len) {
+      //Data recieved
+      KeyframePacket packet;
+      memcpy(&packet, msg, len);
+      KeyFrame frame;
+      frame.degreesToReach = packet.degreesToReach;
+      frame.timeToPosition = packet.timeToPosition;
+      frame.timeSpent = 0;
+      servoAnimations[packet.servoToApplyTo].push(frame);
+      Serial.print(F("\r\nKeyframe received for servo: "));
+      Serial.print(packet.servoToApplyTo);
     }
   }
 }
+
+void sendConfirmation(uint8_t servo)
+{
+  if(connected)
+  {
+    uint8_t confirmationBuffer[1];
+    confirmationBuffer[0] = servo;
+    uint8_t rcode = adk.SndData(1, confirmationBuffer);
+    if (rcode && rcode != hrNAK) {
+      //Data error
+      Serial.print(F("\r\nData send error: "));
+      Serial.print(rcode, HEX);
+    } else if (rcode != hrNAK) {
+      //Data sent
+      Serial.print(F("\r\nConfirmation sent: "));
+      Serial.print(servo);
+    }
+  }
+}
+
+void setupServos()
+{
+  int servoPins[NUMOFSERVOS] = SEVO_PIN_DEFINITION;
+  for(int i = 0; i < NUMOFSERVOS; ++i)
+  {
+    servos[i].attach(servoPins[i]);
+    //Make sure the servos are in the default position
+    servos[i].write(0);
+  }
+}
+
+void processServos()
+{
+  timeBetweenProcessing = millis() - start;
+  for(int i = 0; i < NUMOFSERVOS; ++i)
+  {
+    if(servoAnimations[i].isEmpty() == false)
+    {
+      KeyFrame* keyFrameToUse = &(servoAnimations[i].peek());
+      keyFrameToUse->timeSpent += timeBetweenProcessing;
+      if(keyFrameToUse->timeSpent > keyFrameToUse->timeToPosition)
+      {
+        keyFrameToUse->timeSpent = keyFrameToUse->timeToPosition;
+      }
+      int32_t degreeToUse = map(keyFrameToUse->timeSpent, 0, keyFrameToUse->timeToPosition, 0, MAX_SERVO_ROTATION);
+      servos[i].write(degreeToUse);
+      
+      if(keyFrameToUse->timeSpent == keyFrameToUse->timeToPosition)
+      {
+        servoAnimations[i].pop();
+        sendConfirmation(i);
+      }
+    }
+  }
+  start = millis();
+}
+
--- Waver/app/src/main/java/com/DorsetEggs/waver/main.java
+++ Waver/app/src/main/java/com/DorsetEggs/waver/main.java
@@ -1,5 +1,7 @@
 package com.DorsetEggs.waver;
 
+//TODO: Test throoughly!
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -18,7 +20,7 @@ import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.ToggleButton;
+import java.util.Vector;
 
 import com.android.future.usb.UsbAccessory;
 import com.android.future.usb.UsbManager;
@@ -29,8 +31,25 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
 
 public class main extends ActionBarActivity {
+    //TODO: The following definitions are shared with the device. It may be better to have this established with some initialization communications.
+    public enum ServoTypes {DLARM, ULARM, DRARM, URARM, NUMOFSERVOS}
+    private static final int MAX_SERVO_ROTATION = 180;
+
+    class KeyframePacket {
+        public byte/*uint8_t*/ servoToApplyTo;
+        public byte/*uint8_t*/ degreesToReach; // 0 to 180, 0 is straight down
+        //The max amount of time that an animation keyframe can take is 65 seconds.
+        //We can up it later if we feel like it by upping these variables to uint32_t
+        public short/*uint16_t*/ timeToPosition;//Time in ms to reach this point
+    };
+    //2D vector [Servo][Keyframe]
+    Vector<Vector<KeyframePacket>> servoAnimations = new Vector<Vector<KeyframePacket>>();
+    byte[] servoAnimationInPlay = new byte[ServoTypes.NUMOFSERVOS.ordinal()];
+
     public static final boolean D = BuildConfig.DEBUG; // This is automatically set when building
     private static final String TAG = "WaverActivity"; // TAG is used to debug in Android logcat console
     private static final String ACTION_USB_PERMISSION = "com.DorsetEggs.arduino.waver.robot.USB_PERMISSION";
@@ -43,12 +62,14 @@ public class main extends ActionBarActivity {
     private PendingIntent mPermissionIntent;
     private boolean mPermissionRequestPending;
     TextView connectionStatus;
+    boolean callOngoing;
 
     ConnectedThread mConnectedThread;
     private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         @Override
         //Usb broadcast event
         public void onReceive(Context context, Intent intent) {
+            instantiateAnimation();
             String action = intent.getAction();
             if (ACTION_USB_PERMISSION.equals(action)) {
                 synchronized (this) {
@@ -70,6 +91,53 @@ public class main extends ActionBarActivity {
         }
     };
 
+    private void sendKeyframe(byte servo, int keyframe)
+    {
+        if (mOutputStream != null) {
+            try {
+                servoAnimations.get(servo).get(keyframe);
+                mOutputStream.write(toByteArray(servoAnimations.get(servo).get(keyframe)));
+            } catch (IOException e) {
+                if (D)
+                    Log.e(TAG, "write failed", e);
+            }
+        }
+    }
+
+    public static byte[] toByteArray(Object obj) throws IOException {
+        byte[] bytes = null;
+        ByteArrayOutputStream bos = null;
+        ObjectOutputStream oos = null;
+        try {
+            bos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            bytes = bos.toByteArray();
+        } finally {
+            if (oos != null) {
+                oos.close();
+            }
+            if (bos != null) {
+                bos.close();
+            }
+        }
+        return bytes;
+    }
+
+    private void animationCompleteReceived(byte servo)
+    {
+        if(true == callOngoing) {
+            servoAnimationInPlay[servo] += 1;
+            if(servoAnimationInPlay[servo] >= servoAnimations.get(servo).size())
+            {
+                servoAnimationInPlay[servo] = 0;
+            }
+            sendKeyframe(servo, servoAnimationInPlay[servo]);
+        }
+    }
+
+    // TODO: Test the call receiver functionality fully.
     private final BroadcastReceiver mCallReceiver = new BroadcastReceiver() {
         @Override
         //Call broadcast event
@@ -80,17 +148,26 @@ public class main extends ActionBarActivity {
                 // get the phone number
                 String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
                 printCallReceivedMessage("Call from:" + incomingNumber);
+                callOngoing = true;
+                for(byte i = 0; i < ServoTypes.NUMOFSERVOS.ordinal(); ++i)
+                {
+                    sendKeyframe(i, 0);
+                }
+
             } else if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(
                     TelephonyManager.EXTRA_STATE_IDLE)
                     || intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(
                     TelephonyManager.EXTRA_STATE_OFFHOOK)) {
                 printCallDroppedMessage("Detected call hangup event");
+                callOngoing = false;
+                resetServoAnimations();
             }
         }
     };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        callOngoing = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         connectionStatus = (TextView) findViewById(R.id.connectionStatus);
@@ -227,19 +304,6 @@ public class main extends ActionBarActivity {
         }
     }
 
-    public void blinkLED(View v) {
-        byte buffer = (byte) ((((ToggleButton) v).isChecked()) ? 1 : 0); // Read button
-
-        if (mOutputStream != null) {
-            try {
-                mOutputStream.write(buffer);
-            } catch (IOException e) {
-                if (D)
-                    Log.e(TAG, "write failed", e);
-            }
-        }
-    }
-
     private class ConnectedThread extends Thread {
         Activity activity;
         TextView mTextView;
@@ -256,12 +320,12 @@ public class main extends ActionBarActivity {
             while (running) {
                 try {
                     int bytes = mInputStream.read(buffer);
-                    if (bytes > 3) { // The message is 4 bytes long
+                    if (bytes > 0) { // The message is 1 bytes long
                         activity.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                long timer = ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN).getLong();
-                                mTextView.setText(Long.toString(timer));
+                                byte servo = ByteBuffer.wrap(buffer).order(ByteOrder.LITTLE_ENDIAN).get();
+                                animationCompleteReceived(servo);
                             }
                         });
                     }
@@ -300,15 +364,42 @@ public class main extends ActionBarActivity {
         mDebugOutput.setText(output);
     }
 
-    public void printCallDroppedMessage(String output) {
+    private void printCallDroppedMessage(String output) {
         TextView mCallDroppedInfo;
         mCallDroppedInfo = (TextView) findViewById(R.id.callDroppedInfo);
         mCallDroppedInfo.setText(output);
     }
 
-    public void printCallReceivedMessage(String output) {
+    private void printCallReceivedMessage(String output) {
         TextView mCallRecievedInfo;
         mCallRecievedInfo = (TextView) findViewById(R.id.callRecievedInfo);
         mCallRecievedInfo.setText(output);
+    }
+
+    private void instantiateAnimation()
+    {
+        for(byte i = 0; i < ServoTypes.NUMOFSERVOS.ordinal(); ++i) {
+            Vector<KeyframePacket> servoAnimation = new Vector<KeyframePacket>();
+            KeyframePacket keyframe1 = new KeyframePacket();
+            KeyframePacket keyframe2 = new KeyframePacket();
+
+            keyframe1.servoToApplyTo = i;
+            keyframe1.degreesToReach = 90;
+            keyframe1.timeToPosition = 1000;
+            servoAnimation.add(keyframe1);
+            keyframe2.servoToApplyTo = i;
+            keyframe2.degreesToReach = 0;
+            keyframe2.timeToPosition = 1000;
+            servoAnimation.add(keyframe2);
+            servoAnimations.add(i, servoAnimation);
+        }
+        resetServoAnimations();
+    }
+
+    private void resetServoAnimations()
+    {
+        for(byte i = 0; i < ServoTypes.NUMOFSERVOS.ordinal(); ++i) {
+            servoAnimationInPlay[i] = 0;
+        }
     }
 }
